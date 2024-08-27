ccba892b3b7cc30d6b8922677477def40bfc7138
tomnycui <tomnycui@126.com>
2024-08-27 21:56:46 +0800

    fix some edge cases

--- bin/main.ml
+++ bin/main.ml
@@ -20,15 +20,14 @@ let () =
   in
   let buf = Buffer.create 17 in
   let tmp = Mdtoken.Tokens.add2buffer buf in
-  let dirty_state = ref false in
-  let rec loop last_tok =
+  let rec loop dirty_state last_tok =
     match lexer () with
     | Text T_eof -> tmp last_tok
     | cur_tok ->
       (match last_tok, cur_tok with
        (* pangu *)
        | Text T.T_space, Text T.T_cr | Text T.T_space, Text T.T_space ->
-         cur_tok |> loop
+         cur_tok |> loop dirty_state
        | Text (T.T_zh _), Text (T.T_zh _)
        | Text (T.T_zh _), Text (T.T_punct _)
        | Text (T.T_zh _), Text T.T_space
@@ -37,36 +36,35 @@ let () =
        | Text T.T_space, Text (T.T_zh _)
        | Text T.T_cr, Text (T.T_zh _) ->
          last_tok |> tmp;
-         cur_tok |> loop
+         cur_tok |> loop dirty_state
        | Text (T.T_zh _), _ | _, Text (T.T_zh _) ->
          last_tok |> tmp;
          Text T.T_space |> tmp;
-         cur_tok |> loop
+         cur_tok |> loop dirty_state
        (* depangu *)
        | Math M.T_space, Math M.T_space
        | Math M.T_space, Math M.T_lc
        | Math M.T_space, Math M.T_rc
        | Math M.T_space, Math (M.T_cr _)
-       | Math M.T_space, Math (M.T_num _) -> cur_tok |> loop
+       | Math M.T_space, Math (M.T_num _)
+       | Math M.T_space, Math (M.T_punct _)
+       | Math M.T_space, Math (M.T_cmd _) -> cur_tok |> loop dirty_state
        | Math M.T_space, Math _ ->
-         if !dirty_state
+         if dirty_state
          then (
            last_tok |> tmp;
-           dirty_state := false)
-         else ();
-         cur_tok |> loop
+           cur_tok |> loop false)
+         else cur_tok |> loop dirty_state
        | Math (M.T_cmd _), Math _ ->
          last_tok |> tmp;
-         dirty_state := true;
-         cur_tok |> loop
+         cur_tok |> loop true
        | Math _, Math _ ->
          last_tok |> tmp;
-         dirty_state := false;
-         cur_tok |> loop
+         cur_tok |> loop false
        | _ ->
          last_tok |> tmp;
-         cur_tok |> loop)
+         cur_tok |> loop dirty_state)
   in
-  lexer () |> loop;
+  lexer () |> loop false;
   Buffer.output_buffer stdout buf
 ;;
